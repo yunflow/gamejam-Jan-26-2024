060fdd31b624a8a623ade5cf8a028bd16ad2f4dc
Hunter <hunterzhou2000@gmail.com>
2024-01-27 12:23:29 -0400

    health bar UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,22 +5,37 @@ namespace UI
 {
     public class UIManager : MonoBehaviour
     {
-        [SerializeField] private Slider progressBar;
+        [SerializeField] private Image progressBar;
         [SerializeField] private Image progressBarImage;
+
+        [SerializeField] private float testHealth = 3;
 
         private void Start()
         {
-            progressBar.value = 0;
+            // progressBar.value = 0;
         }
 
         private void Update()
         {
             UpdateProgressBar();
+
+            if (Input.GetKeyDown(KeyCode.F)) {
+                testHealth--;
+            }
         }
 
         private void UpdateProgressBar()
         {
-
+            progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, testHealth / 3, 0.05f);
+            if (testHealth == 3) {
+                progressBar.color = Color.green;
+            } else if (testHealth == 2) {
+                progressBar.color = Color.yellow;
+            } else {
+                progressBar.color = Color.red;
+            }
+            
+            
         }
     }
 }
