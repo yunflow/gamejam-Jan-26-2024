f45a4e650168d2e9f8c84442371fb8def44edd52
Hunter <hunterzhou2000@gmail.com>
2024-01-28 14:25:10 -0400

    background added

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -36,7 +36,7 @@ namespace UI
         {
             progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, tlm.points / 3, 0.05f);
             if (tlm.points == 3) {
-                progressBar.color = Color.green;
+                // progressBar.color = Color.green;
             } else if (tlm.points == 2) {
                 progressBar.color = Color.yellow;
             } else {
